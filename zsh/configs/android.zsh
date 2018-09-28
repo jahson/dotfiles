0611b3258eee6119bd111af86c42a2e89b1bf6e4
jahson <jjahson@gmail.com>
2018-09-28 21:58:48 +0800

    Add android conf

---
+++ zsh/configs/android.zsh
@@ -0,0 +1,5 @@
+# export ANT_HOME=/usr/local/opt/ant
+# export MAVEN_HOME=/usr/local/opt/maven
+# export GRADLE_HOME=/usr/local/opt/gradle
+export ANDROID_HOME=/usr/local/share/android-sdk
+# export ANDROID_NDK_HOME=/usr/local/share/android-ndk
